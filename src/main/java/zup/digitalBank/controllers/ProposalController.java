3f4563deab6da1db008130064cf30390cf39974d
Alessandra <alessandraamontanher@gmail.com>
2020-10-20 22:24:06 -0300

    implements service and responses

--- src/main/java/zup/digitalBank/controllers/ProposalController.java
+++ src/main/java/zup/digitalBank/controllers/ProposalController.java
@@ -1,20 +1,38 @@
 package zup.digitalBank.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import zup.digitalBank.models.CustomerPersonalDetail;
+import zup.digitalBank.services.ProposalService;
 
 import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/proposal")
 public class ProposalController {
 
+    @Autowired
+    private ProposalService proposalService;
+
     @PostMapping("/init")
-    public String create(@Valid @RequestBody CustomerPersonalDetail customerpersonalDetail) {
-        return "OK";
+    public ResponseEntity<CustomerPersonalDetail> create(@Valid @RequestBody CustomerPersonalDetail customerpersonalDetail) throws URISyntaxException {
+        boolean proposalCreated = proposalService.createInitProposal(customerpersonalDetail);
+
+        if(proposalCreated){
+            HttpHeaders responseHeaders = new HttpHeaders();
+            responseHeaders.setLocation(new URI("http://localhost:8080/proposal/init"));
+            return new ResponseEntity(responseHeaders, HttpStatus.CREATED);
+        }else{
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(customerpersonalDetail);
+        }
     }
 }
---
+++ src/main/java/zup/digitalBank/services/ProposalService.java
@@ -0,0 +1,12 @@
+package zup.digitalBank.services;
+
+import org.springframework.stereotype.Service;
+import zup.digitalBank.models.CustomerPersonalDetail;
+
+@Service
+public class ProposalService {
+
+    public boolean createInitProposal(CustomerPersonalDetail customerPersonalDetail){
+        return false;
+    }
+}
