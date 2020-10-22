f2d9fb9e2549e3052d6cb276283acbbd99fb9b5e
alessandra <alessandraamontanher@gmail.com>
2020-10-22 12:53:47 -0300

    Adjusting location

--- src/main/java/zup/digitalBank/services/ProposalService.java
+++ src/main/java/zup/digitalBank/services/ProposalService.java
@@ -1,11 +1,14 @@
 package zup.digitalBank.services;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import zup.digitalBank.models.Customer;
-import zup.digitalBank.models.CustomerPersonalDetail;
-import zup.digitalBank.models.Proposal;
+import zup.digitalBank.models.*;
 import zup.digitalBank.repositories.ProposalRepository;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.UUID;
@@ -62,5 +65,34 @@ public class ProposalService {
 
     public Proposal getProposal(UUID proposalId) throws SQLException {
         return proposalRepository.getProposal(proposalId);
+    }
+
+    public ResponseEntity createProposalAddress(CustomerAddress customerAddress, String proposalId, ProposalStep proposalStep) throws SQLException, URISyntaxException {
+        try{
+            boolean previousStepIsOk = CheckIfPreviousProposalStepIsOk(proposalId, proposalStep);
+
+            if(previousStepIsOk){
+                Proposal customerProposal = getProposal(UUID.fromString(proposalId));
+                customerService.createCustomerAddress(customerAddress, customerProposal.getCustomerId());
+
+                String uri = String.format("http://localhost:8080/proposal/3/%s/document",proposalId);
+                HttpHeaders responseHeaders = getHeader(uri);
+                return new ResponseEntity(responseHeaders, HttpStatus.CREATED);
+            }else{
+                return new ResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY);
+            }
+        }catch (SQLException | URISyntaxException exception){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(customerAddress);
+        }
+    }
+
+    private boolean CheckIfPreviousProposalStepIsOk(String proposalId, ProposalStep proposalStep) throws SQLException {
+        return proposalRepository.checkIfPreviousProposalStepIsOk(proposalId, proposalStep.ordinal());
+    }
+
+    private HttpHeaders getHeader(String location) throws URISyntaxException {
+        HttpHeaders responseHeaders = new HttpHeaders();
+        responseHeaders.setLocation(new URI(location));
+        return responseHeaders;
     }
 }
